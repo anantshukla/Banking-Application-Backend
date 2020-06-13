b507e3fb47e57b43611550eb5307e44e42591fcf
anand_kumar96@hotmail.com <anand_kumar96@hotmail.com>
2020-06-14 03:58:23 +0530

    API for create customer, search customer and update customer has done

--- retailbank/models.py
+++ retailbank/models.py
@@ -1,5 +1,5 @@
 import flask
-from retailbank import db
+from retailbank import db,ma
 
 ############### Creating database model ###########################
 
@@ -8,4 +8,58 @@ class Userstore(db.Model):
     password=db.Column(db.String(45),unique=False, nullable=False)
     time = db.Column(db.String(120), unique=False,nullable=False)
     token = db.Column(db.String(200),unique=True,nullable=True)
-    user_role = db.Column(db.String(10),unique=False,nullable=False)
+    user_role = db.Column(db.String(30),unique=False,nullable=False)
+
+
+class Customer(db.Model):
+	ws_ssn=db.Column(db.Integer,unique=True, nullable=False)
+	ws_cust_id=db.Column(db.Integer,primary_key=True,autoincrement=True)
+	ws_name=db.Column(db.String(20),unique=False,nullable=False)
+	ws_adrs=db.Column(db.String(200),unique=False,nullable=False)
+	ws_age=db.Column(db.Integer,unique=False,nullable=False)
+	accounts=db.relationship('Account',backref='accountof')
+	c_status=db.relationship('Customer_status',backref='cstatus')
+	a_status=db.relationship('Account_status',backref='astatus')
+	trans=db.relationship('Transcation',backref='ctrans')
+
+class Account(db.Model):
+	ws_cust_id=db.Column(db.Integer,db.ForeignKey('customer.ws_cust_id'))
+	ws_acct_id=db.Column(db.Integer, primary_key=True)
+	ws_acct_type=db.Column(db.String(1),unique=False,nullable=False)
+	ws_acct_balance=db.Column(db.Float,unique=False,nullable=False)
+	ws_acct_crdate=db.Column(db.String(12),unique=False,nullable=False)
+	ws_acct_lasttrdate=db.Column(db.String(12),unique=False,nullable=False)
+	ws_acct_duration=db.Column(db.Integer,unique=False,nullable=False)
+	ac_status=db.relationship('Account_status',backref='hasacstatus')
+
+class Transcation(db.Model):
+	ws_cust_id=db.Column(db.Integer,db.ForeignKey('customer.ws_cust_id'))
+	ws_acct_type=db.Column(db.String(1),unique=False,nullable=False)
+	ws_amt=db.Column(db.Float,unique=False,nullable=False)
+	ws_trxn_date=db.Column(db.String(12),unique=False,nullable=False)
+	ws_src_typ=db.Column(db.String(1),unique=False,nullable=False)
+	ws_tgt_typ=db.Column(db.String(1),unique=False,nullable=False)
+	trxn_id=db.Column(db.Integer,primary_key=True)
+
+class Customer_status(db.Model):
+	ws_ssn=ws_ssn=db.Column(db.Integer,unique=True, nullable=False)
+	ws_cust_id=db.Column(db.Integer,db.ForeignKey('customer.ws_cust_id'))
+	ws_status=db.Column(db.Integer,unique=False,nullable=False)
+	ws_msg=db.Column(db.String(45),unique=False,nullable=False)
+	ws_lup=db.Column(db.String(120),unique=False,nullable=False)
+	cust_status_index=db.Column(db.Integer,primary_key=True)
+
+class Account_status(db.Model):
+	ws_cust_id=db.Column(db.Integer,db.ForeignKey('customer.ws_cust_id'))
+	ws_acct_id=db.Column(db.Integer,db.ForeignKey('account.ws_acct_id'))
+	ws_cust_typ=db.Column(db.String(1),unique=False,nullable=False)
+	ws_cust_msg=db.Column(db.String(45),unique=False,nullable=False)
+	ws_cust_lup=db.Column(db.String(120),unique=False,nullable=False)
+	acct_status_index=db.Column(db.Integer,primary_key=True)    
+
+class CustomerSchema(ma.Schema):
+    class Meta:
+        fields = ('ws_ssn','ws_cust_id','ws_name','ws_adrs','ws_age')
+
+customer_schema = CustomerSchema()        
+
--- retailbank/route.py
+++ retailbank/route.py
@@ -1,6 +1,6 @@
 from flask import Flask,jsonify,request
 from retailbank import app,db
-from retailbank.models import Userstore
+from retailbank.models import Userstore, Customer, Customer_status,customer_schema
 import datetime
 import uuid
 
@@ -11,14 +11,88 @@ def login():
     data = request.get_json()
     username=int(data["username"])
     password=data["password"]
-    user = Userstore.query.filter_by(login_id=username,password=password).first()   #checking credencial
+    user = Userstore.query.filter_by(login_id=username,password=password).first()   # checking credencial
     if user:
-        if user.token=="" or user.token==None:    #checking if user if login first time
-            token=uuid.uuid4()   #generating token and storing into database
+        if user.token=="" or user.token==None:    # checking if user if login first time
+            token=uuid.uuid4()  # generating token and storing into database
             user.token=str(token)
             db.session.commit()
-            return jsonify({"status":True,"role":user.user_role,"token":token})    #reponding with user role and token
+            return jsonify({"status":True,"role":user.user_role,"token":token})    # reponding with user role and token
         else:
             return jsonify({"status":True,"role":user.user_role,"token":user.token})   
     else:
-        return jsonify({"error":"failed"}),401      #responding with error 401
+        return jsonify({"status":False,"message":"Something went wrong! Please check your credential"})     #responding with error
+
+############# API for creating Customer #################
+
+@app.route("/createcustomer", methods=["POST"])
+def createcustomer():
+    data = request.get_json()
+    token = data["token"]
+    user = Userstore.query.filter_by(token=token).first()       # Checking for authentication using token
+    if user:
+        if user.user_role =='account executive':    # Checking authorization with user_role
+            ssn = data["ssn"]
+            if Customer.query.filter_by(ws_ssn=ssn).first():    # Checking for duplicate ssn
+                return jsonify({"status":False,"messaage":f"Customer with ssn {ssn} already exist"})
+            cust_id = Customer.query.count()+1
+            name = data["name"]
+            age = data["age"]
+            address = data["address"]+","+data["city"]+","+data["state"]
+            cust = Customer(ws_ssn=ssn,ws_cust_id=cust_id,ws_name=name,ws_adrs=address,ws_age=age)
+            db.session.add(cust)        # Saving customer in database
+            cust_status_index = Customer_status.query.count()+1
+            cust_status = Customer_status(ws_ssn=ssn, ws_cust_id=cust_id, ws_status=1, ws_msg="Customer created successfully",
+            ws_lup=datetime.datetime.now(), cust_status_index=cust_status_index)
+            db.session.add(cust_status)     # Saving customer status in database
+            db.session.commit()
+            return jsonify({"status":True,"message":"Customer created successfully"})
+        return jsonify({'status':False,'message':'Not authorized'})
+    return jsonify({'status':False,'message':'Not authenticated'})
+
+###############  API for customer search ##################
+
+@app.route("/customersearch", methods=["POST"])
+def customersearch():
+    data = request.get_json()
+    token = data["token"]
+    user = Userstore.query.filter_by(token=token).first()    # Checking for authentication with token
+    if user:
+        if user.user_role =='account executive':    # Checking authorization with user_role
+            if data.get('type')=='ssn':         # Checking search type
+                customer = Customer.query.filter_by(ws_ssn=int(data.get('id'))).first() # Query to featch data
+                result = customer_schema.dump(customer)     # Serializing the database result
+                return jsonify({"status":True, "result":result})
+            customer = Customer.query.filter_by(ws_cust_id=int(data.get('id'))).first()
+            result = customer_schema.dump(customer)
+            return jsonify({"status":True, "result":result})
+        return jsonify({'status':False,'message':'Not authorized'})            
+    return jsonify({'status':False,'message':'Not authenticated'})
+
+
+@app.route("/customerupdate", methods=["POST"])
+def customerupdate():
+    data = request.get_json()
+    token = data["token"]
+    user = Userstore.query.filter_by(token=token).first()    # Checking for authentication with token
+    if user:
+        if user.user_role =='account executive':    # Checking authorization with user_role
+            customer = Customer.query.filter_by(ws_cust_id=data.get('ws_cust_id')).first()
+            customer.ws_name=data.get('ws_name')
+            customer.ws_age=data.get('ws_age')
+            customer.ws_adrs=data.get('ws_adrs')    # Updating customer details
+            cust_status_index = Customer_status.query.count()+1
+            cust_status = Customer_status(ws_ssn=data.get('ws_ssn'), ws_cust_id=data.get('ws_cust_id'), ws_status=1, ws_msg="Customer update complete",
+            ws_lup=datetime.datetime.now(), cust_status_index=cust_status_index)
+            db.session.add(cust_status)     # Creating customer status
+            db.session.commit()     
+            return jsonify({"status":True,"messgae":"Customer update complete"})
+        return jsonify({'status':False,'message':'Not authorized'})            
+    return jsonify({'status':False,'message':'Not authenticated'})
+
+
+
+
+
+
+
